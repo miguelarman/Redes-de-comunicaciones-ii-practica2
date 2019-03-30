32128ec0c1140d795f966b3662f42cfd6a23be9b
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-03-30 18:36:48 +0100

    Borrados los ficheros de prueba de los distintos modulos

--- test_funciones_cripto.py
+++
@@ -1,7 +0,0 @@
-from securebox_crypto import encript, decrypt
-
-
-ruta_encriptado = encript('rsa/publica.pem', 'fichero_para_cifrar', 'fichero_cifrado.bin', verbose=True)
-
-
-ruta_desencriptado = decrypt('rsa/privada.pem', 'fichero_cifrado.bin', 'fichero_descifrado', verbose=True)
