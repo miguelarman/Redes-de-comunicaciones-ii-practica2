e030b665f5a9e5839a42f1bbb46e550addaf5ff8
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-10 23:31:30 +0200

    añadidos comentarios a los ficheros generales y credenciales

--- generales.py
+++ generales.py
@@ -1,3 +1,15 @@
+"""
+    Prácticas de Redes de comunicaciones 2
+
+    Autores:
+        Miguel Arconada Manteca
+        Mario García Pascual
+
+    generales.py:
+        Este fichero contiene algunas constantes útiles para el resto de
+        módulos.
+"""
+
 import credenciales
 
 url_servidor = 'http://vega.ii.uam.es:8080'
