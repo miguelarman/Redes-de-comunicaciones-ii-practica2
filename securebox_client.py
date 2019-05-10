b11176f0a9c2741b09c89abc89d4d1f55df3f4ae
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-10 23:31:08 +0200

    añadidos comentarios a securebox_client

--- securebox_client.py
+++ securebox_client.py
@@ -1,4 +1,17 @@
+"""
+    Prácticas de Redes de comunicaciones 2
+
+    Autores:
+        Miguel Arconada Manteca
+        Mario García Pascual
+
+    securebox_client:
+        Este es el fichero que contiene el programa del cliente de SecureBox,
+        el que se tiene que ejecutar.
+"""
+
 from securebox_options import parse_options, process_options
 
-opts = parse_options()
-process_options(opts)
+if __name__ == '__main__':
+    opts = parse_options()
+    process_options(opts)
